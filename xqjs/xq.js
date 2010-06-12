30a84dedd3fdd5967350172c642e5af52f641415
satyr <murky.satyr@gmail.com>
2010-06-12 18:28:44 +0900

    moved xul-independent funcs to xqc.js

--- xqjs/xq.js
+++ xqjs/xq.js
@@ -1,77 +1,8 @@
-const O2S = Object.prototype.toString;
-const ELLIPSIS = Preferences.get('intl.ellipsis', '\u2026');
-const NS = {
-  x: 'http://www.mozilla.org/keymaster/gatekeeper/there.is.only.xul',
-  e: 'http://www.mozilla.org/2004/em-rdf#',
-  a: 'http://www.w3.org/2005/Atom',
-  s: 'http://www.w3.org/2000/svg',
-  r: 'http://www.w3.org/1999/02/22-rdf-syntax-ns#',
-  h: 'http://www.w3.org/1999/xhtml',
-  m: 'http://www.w3.org/1998/Math/MathML',
-};
-
 var __ = [], cur = 0, utils =
-[function p(x)(say(inspect(x)), x),
- function say(s){
-   var {editor} = results;
-   editor.beginningOfDocument();
-   editor.QueryInterface(Ci.nsIPlaintextEditor).insertText(s +'\n');
-   editor.beginningOfDocument();
-   results.inputField.scrollTop = 0
-   return s;
- },
- function log(s)(Services.console.logStringMessage('xqjs: '+ s), s),
- function sum(a){
-   for each(let key in Array.slice(arguments, 1))
-     a = Array.map(a, typeof key === 'function' ? key : function(x) x[key], a);
-   return a.reduce(function(x, y) x + y);
- },
- function copy(s)(
-   s && (Cc['@mozilla.org/widget/clipboardhelper;1']
-         .getService(Ci.nsIClipboardHelper).copyString(s)),
-   s),
- function type(x) x == null ? '' : O2S.call(x).slice(8, -1),
- function keys(x){
-   try { var it = x && new Iterator(x, true) } catch([]){ return [] }
-   return [k for(k in it)];
- },
- function xmls(x) XMLSerializer().serializeToString(x),
- XPCNativeWrapper.unwrap || function unwrap(x){
-   try { return new XPCNativeWrapper(x).wrappedJSObject }
-   catch([]){ return x }
- },
- function xpath(xp, doc, one){
-   if(typeof doc !== 'object') one = doc, doc = 0;
-   doc = doc || target.win.document;
-   var r = doc.evaluate(
-     xp, doc, function([c]) NS[c], XPathResult.ANY_TYPE, null);
-   switch(r.resultType){
-     case r.STRING_TYPE: return r.stringValue;
-     case r.NUMBER_TYPE: return r.numberValue;
-     case r.BOOLEAN_TYPE: return r.booleanValue;
-     case r.UNORDERED_NODE_ITERATOR_TYPE:
-     if(one) return r.iterateNext();
-     let a = [], n;
-     while((n = r.iterateNext())) a.push(n);
-     return a;
-   }
- },
- function domi(x)(
-   main()[x && x.nodeType ? 'inspectDOMNode' : 'inspectObject'](x), x),
- function fbug(){
-   var {Firebug} = main(), args = Array.slice(arguments);
-   if(Firebug.Console.isEnabled() && Firebug.toggleBar(true, 'console'))
-     Firebug.Console.logFormatted(args);
-   return args;
- },
- function target(win)(
-   target.win = win = win && win.document ? win : opener || self,
-   target.chrome = chromep(win),
-   target.sb = sandbox(win),
-   document.title = 'xqjs'+ (win === self ? '' : ': '+ fmtitle(win))),
- ];
-for each(let f in utils) this[f.name] = f;
-utils.push(hurl);
+[p, say, target, hurl,
+ log, copy, domi, fbug,
+ xmls, xpath,
+ sum, type, keys, unwrap];
 
 [function bin() JSON.parse(prefs.get('history', '[]')),
  function CoffeeScript() Cu.import('resource://xqjs/coffee.jsm').CoffeeScript,
@@ -110,6 +41,14 @@ function onload(){
   code.focus();
 }
 
+function target(win){
+  target.win = win = win && win.document ? win : opener || self;
+  target.chrome = chromep(win);
+  target.sb = sandbox(win);
+  document.documentElement.setAttribute('target', win.location);
+  document.title = 'xqjs'+ (win === self ? '' : ': '+ fmtitle(win));
+  return win;
+}
 function execute(){
   code.focus();
   var js = expand(save(code.value)), cn = '';
@@ -159,9 +98,31 @@ function macload(){
   }
   self.macrun = m;
 }
-function chromep(win){
-  try { return !!Cu.evalInSandbox('Components.utils', Cu.Sandbox(win)) }
-  catch([]){ return false }
+
+function p(x)(say(inspect(x)), x);
+function say(s){
+  var {editor} = results;
+  editor.beginningOfDocument();
+  editor.QueryInterface(Ci.nsIPlaintextEditor).insertText(s +'\n');
+  editor.beginningOfDocument();
+  results.inputField.scrollTop = 0
+    return s;
+}
+function xpath(xp, doc, one){
+  if(typeof doc !== 'object') one = doc, doc = 0;
+  doc = doc || target.win.document;
+  var r = doc.evaluate(
+    xp, doc, function([c]) NS[c], XPathResult.ANY_TYPE, null);
+  switch(r.resultType){
+    case r.STRING_TYPE: return r.stringValue;
+    case r.NUMBER_TYPE: return r.numberValue;
+    case r.BOOLEAN_TYPE: return r.booleanValue;
+    case r.UNORDERED_NODE_ITERATOR_TYPE:
+    if(one) return r.iterateNext();
+    let a = [], n;
+    while((n = r.iterateNext())) a.push(n);
+    return a;
+  }
 }
 
 function copand() say(copy(expand(code.value)));
@@ -237,51 +198,6 @@ function wordig(re){
     yield results.value;
     for each(var s in bin) yield s;
   }) while([word] = re(s) || 0) word in dic || (yield dic[word] = word);
-}
-
-function inspect(x){
-  if(x == null) return String(x);
-  var t = typeof x;
-  switch(t){
-    case 'object': case 'function': break;
-    case 'string': return x;
-    case 'xml': x = x.toXMLString();
-    default: return x +'  '+ t;
-  }
-  var os = O2S.call(x), t = os.slice(8, -1);
-  switch(t){
-    case 'Function': return x.toSource(0);
-    case 'XPCNativeWrapper': case 'XPCCrossOriginWrapper':
-    let wos = O2S.call(unwrap(x));
-    os = t[3] === 'N' ? '[object '+ t +' '+ wos +']' : wos;
-    t += ':'+ wos.slice(8, -1);
-  }
-  var s, nt = x.nodeType;
-  if(nt === 1) s = xmls(x.cloneNode(0)).replace(/ xmlns=".+?"/, '');
-  else if(nt) s = x.nodeValue;
-  if(s == null){
-    try { s = String(x) } catch(e){
-      x.__proto__ ? Cu.reportError(e) : t = 'Null';
-      s = os;
-    }
-    if(s === os) s = '{'+ keys(unwrap(x)).join(', ') +'}';
-  }
-  return s +'  '+ t;
-}
-function fmtitle(win){
-  const LEN = 80;
-  var ttl = win.document.title.trim();
-  var url = win.location.href.replace(/^http:\/+/, '');
-  if(!ttl) return ellipsize(url, LEN);
-  ttl = ellipsize(ttl, LEN/2, true);
-  return ttl + ' <'+ ellipsize(url, LEN - ttl.length) +'>';
-}
-function ellipsize(str, num, end){
-  if(num < 1) return '';
-  if(str.length <= num) return str;
-  if(end) return str.slice(0, num - 1) + ELLIPSIS;
-  var i = num / 2;
-  return str.slice(0, num - i) + ELLIPSIS + str.slice(str.length - i + 1);
 }
 
 function fillwin(menu){
--- xqjs/xqc.js
+++ xqjs/xqc.js
@@ -2,6 +2,17 @@ const {classes: Cc, interfaces: Ci, utils: Cu, results: Cr} = Components;
 Cu.import('resource://xqjs/Services.jsm');
 Cu.import('resource://xqjs/Preferences.jsm');
 
+const O2S = Object.prototype.toString;
+const NS = {
+  x: 'http://www.mozilla.org/keymaster/gatekeeper/there.is.only.xul',
+  e: 'http://www.mozilla.org/2004/em-rdf#',
+  a: 'http://www.w3.org/2005/Atom',
+  s: 'http://www.w3.org/2000/svg',
+  r: 'http://www.w3.org/1999/02/22-rdf-syntax-ns#',
+  h: 'http://www.w3.org/1999/xhtml',
+  m: 'http://www.w3.org/1998/Math/MathML',
+};
+
 var prefs = new Preferences('extensions.xqjs.');
 
 function qs(s) document.querySelector(s);
@@ -15,11 +26,92 @@ function empty(lm){
   while(lm.hasChildNodes()) lm.removeChild(lm.lastChild);
   return lm;
 }
+function xmls(node) XMLSerializer().serializeToString(node);
+
 function main() Services.wm.getMostRecentWindow('navigator:browser');
 function hurl() let(b = main().gBrowser) b.addTab.apply(b, arguments);
+function chromep(win){
+  try { return !!Cu.evalInSandbox('Components.utils', Cu.Sandbox(win)) }
+  catch([]){ return false }
+}
+function fmtitle(win){
+  const LEN = 80;
+  var ttl = win.document.title.trim();
+  var url = win.location.href.replace(/^http:\/+/, '');
+  if(!ttl) return ellipsize(url, LEN);
+  ttl = ellipsize(ttl, LEN/2, true);
+  return ttl + ' <'+ ellipsize(url, LEN - ttl.length) +'>';
+}
+
+function log(s)(Services.console.logStringMessage('xqjs: '+ s), s);
+function copy(s)(
+  s && (Cc['@mozilla.org/widget/clipboardhelper;1']
+        .getService(Ci.nsIClipboardHelper).copyString(s)),
+  s);
+function domi(x)(
+  main()[x && x.nodeType ? 'inspectDOMNode' : 'inspectObject'](x), x);
+function fbug(){
+  var {Firebug} = main(), args = Array.slice(arguments);
+  if(Firebug.Console.isEnabled() && Firebug.toggleBar(true, 'console'))
+    Firebug.Console.logFormatted(args);
+  return args;
+}
+
+var unwrap = XPCNativeWrapper.unwrap || function unwrap(x){
+  try { return new XPCNativeWrapper(x).wrappedJSObject }
+  catch([]){ return x }
+};
+function type(x) x == null ? '' : O2S.call(x).slice(8, -1);
 function lazy(o, fn, p){
   if(typeof p !== 'string') p = fn.name;
   o.__defineGetter__(p, function() o[p] = delete o[p] && fn.call(o));
   return o;
 }
+function sum(a){
+  for each(let key in Array.slice(arguments, 1))
+    a = Array.map(a, typeof key === 'function' ? key : function(x) x[key], a);
+  return a.reduce(function(x, y) x + y);
+}
+function keys(x){
+  try { var it = x && new Iterator(x, true) } catch([]){ return [] }
+  return [k for(k in it)];
+}
+function inspect(x){
+  if(x == null) return String(x);
+  var t = typeof x;
+  switch(t){
+    case 'object': case 'function': break;
+    case 'string': return x;
+    case 'xml': x = x.toXMLString();
+    default: return x +'  '+ t;
+  }
+  var os = O2S.call(x), t = os.slice(8, -1);
+  switch(t){
+    case 'Function': return x.toSource(0);
+    case 'XPCNativeWrapper': case 'XPCCrossOriginWrapper':
+    let wos = O2S.call(unwrap(x));
+    os = t[3] === 'N' ? '[object '+ t +' '+ wos +']' : wos;
+    t += ':'+ wos.slice(8, -1);
+  }
+  var s, nt = x.nodeType;
+  if(nt === 1) s = xmls(x.cloneNode(0)).replace(/ xmlns=".+?"/, '');
+  else if(nt) s = x.nodeValue;
+  if(s == null){
+    try { s = String(x) } catch(e){
+      x.__proto__ ? Cu.reportError(e) : t = 'Null';
+      s = os;
+    }
+    if(s === os) s = '{'+ keys(unwrap(x)).join(', ') +'}';
+  }
+  return s +'  '+ t;
+}
+
 function rescape(s) String(s).replace(/[.?*+^$|()\{\[\\]/g, '\\$&');
+function ellipsize(str, num, end){
+  if(num < 1) return '';
+  if(str.length <= num) return str;
+  const ELLIPSIS = Preferences.get('intl.ellipsis', '\u2026');
+  if(end) return str.slice(0, num - 1) + ELLIPSIS;
+  var i = num / 2;
+  return str.slice(0, num - i) + ELLIPSIS + str.slice(str.length - i + 1);
+}
